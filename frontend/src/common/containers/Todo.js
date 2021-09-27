c3bfebdd986db6b016658412d639f3dca3e05182
Juuhyunn <ahnju96@gmail.com>
2021-09-27 17:56:35 +0900

    27, September

--- frontend/src/common/containers/Todo.js
+++ frontend/src/common/containers/Todo.js
@@ -1,15 +1,32 @@
 import React, { useState } from 'react'
-import Button from '@mui/material/Button';
+import {TextField, Button} from '@mui/material';
 import styled from 'styled-components'
 
 
 export default function Todo () {
-    const [todo, setTodo] = useState(0)
-    return(<><Div> 
-    {todo == 1 && <p>안녕하세요</p>}
-    <Button variant="contained" style={{margin:'10px'}} onClick={()=> setTodo(1)}>Show</Button>
-    <Button variant="contained" style={{margin:'10px'}} onClick={()=> setTodo(0)}>Hide</Button>
-    </Div></>)
+    const [todo, setTodo] = useState('')
+    let val = ''
+    const add = e => {
+        e.preventDefault()
+        val = e.target.value
+    }
+    const del = e => {
+        e.preventDefault()
+        setTodo('')
+    }
+    const submitForm = e => {
+        e.preventDefault()
+        setTodo(val)
+        document.getElementById('todo-input').value = '' //text input 의 데이터를 리셋시켜준다
+    }
+    return(<><Div><form onSubmit={submitForm} method='POST'>
+    <h1>할 일 목록</h1>
+    <input type='text' id='todo-input' onChange={add}/>
+    <input type='submit' value = 'ADD'/>
+    <br/>
+    {todo != '' && <div><span>{todo}</span>
+    <input type='submit' value = 'Del' onClick={del}/></div>}
+    </form></Div></>)
 }
 
 const Div = styled.div`
