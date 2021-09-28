ad49035233d4f1793a5e5ec361bae452c3c884d8
Juuhyunn <ahnju96@gmail.com>
2021-09-28 18:18:00 +0900

    28, September

--- frontend/src/common/components/TodoList.js
+++ frontend/src/common/components/TodoList.js
@@ -3,15 +3,24 @@ import styled from 'styled-components'
 import Alert from '@mui/material/Alert';
 import AlertTitle from '@mui/material/AlertTitle';
 import Stack from '@mui/material/Stack';
+import { useSelector } from "react-redux";
 
 export default function TodoList() {
+    const todos = useSelector( state => state.todos )
     return(<>
     <Div>
+        {todos == null &&
     <Stack sx={{ width: '300px;','margin': '0 auto'}} spacing={2}>
     <Alert severity="warning">
     <AlertTitle>등록된 할 일 목록이 없습니다.</AlertTitle>
     </Alert>
-    </Stack>
+    </Stack> }
+    {todos != null &&
+    <Stack sx={{ width: '300px;','margin': '0 auto'}} spacing={2}>
+    <Alert severity="warning">
+    <AlertTitle>{todos.length}개의 할일 목록이 있습니다</AlertTitle>
+    </Alert>
+    </Stack> }
     </Div>
     </>)
 }
