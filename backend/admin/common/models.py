fe498bd7fa822fb84411eb113298d86838f9624e
Juuhyunn <ahnju96@gmail.com>
2021-10-06 14:21:00 +0900

    6, October

--- backend/admin/common/models.py
+++ backend/admin/common/models.py
@@ -1,6 +1,10 @@
+from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass
+
+import googlemaps
 from icecream import ic
 import pandas as pd
+import json
 
 @dataclass
 class DFrameGenerator(object):
@@ -9,6 +13,9 @@ class DFrameGenerator(object):
     id: str
     label: str
     fname: object
+    context: str
+    url: str
+    dframe: object
 
     @property
     def fname(self) -> object: return self._fname
@@ -30,6 +37,19 @@ class DFrameGenerator(object):
     def label(self) -> str: return self._label
     @label.setter
     def label(self, label): self._label = label
+
+    @property
+    def context(self) -> str: return self._context
+    @context.setter
+    def context(self, context): self._context = context
+    @property
+    def url(self) -> str: return self._url
+    @url.setter
+    def url(self, url): self._url = url
+    @property
+    def dframe(self) -> object: return self._dframe
+    @dframe.setter
+    def dframe(self, dframe): self._dframe = dframe
 
     def create_model(self):
         return pd.read_csv(self.fname)
@@ -61,3 +81,58 @@ Data columns (total 10 columns):
  9   ocean_proximity     20640 non-null  object
 
 '''
+
+
+class ReaderBase(metaclass=ABCMeta):
+    @abstractmethod
+    def new_file(self):
+        pass
+
+    @abstractmethod
+    def csv(self):
+        pass
+
+    @abstractmethod
+    def xls(self):
+        pass
+
+    @abstractmethod
+    def json(self):
+        pass
+
+
+class PrinterBase(metaclass=ABCMeta):
+    @abstractmethod
+    def dframe(self):
+        pass
+
+
+
+class Reader(ReaderBase):
+    def new_file(self, file) -> str:
+        return file.context + file.fname
+
+    def csv(self, file) -> object:
+        return pd.read_csv(f'{self.new_file(file)}.csv', encoding='UTF-8', thousands=',')
+
+    def csv_header(self, file, header) -> object:
+        return pd.read_csv(f'{self.new_file(file)}.csv', encoding='UTF-8', thousands=',', header=header)
+
+    def xls(self, file, header, usecols) -> object:
+        return pd.read_excel(f'{self.new_file(file)}.xls', header=header, usecols=usecols)
+
+    def json(self, file) -> object:
+        # return json.load(open(f'{self.new_file(file)}json', encoding='UTF-8'))
+        return pd.read_json(f'{self.new_file(file)}.json', encoding='UTF-8')
+
+    # pip install googlemaps
+    def gmaps(self) -> object:
+        return googlemaps.Client(key='')
+
+
+class Print(PrinterBase):
+    def dframe(self, this):
+        ic(this.head(3))
+        ic(this.tail(3))
+        ic(this.columns())
+        ic(this.isnull().sum())
---
+++ backend/admin/crime/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+# 모델은 정형화 되어 있는 샘플 데이터(진짜 데이터) 를 가지고 있는 데이터프레임
+# 데이터를 가지고 있지 않는 데이터 프레임은 데이터 스트럭쳐?
+# Create your models here.
+
+
+class CrimeCctvMode():
+    pass
+
+
